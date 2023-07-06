2d963d5e803e33a8787d3498242dd3e7ddb758cb
vstarchevskyi <v.starchevskyi@zakaz.global>
2023-07-06 12:24:34 +0300

    work pc

--- src/ua/hillel/homeworks/hm6/Task13LoopIf1_2_3.java
+++ src/ua/hillel/homeworks/hm6/Task13LoopIf1_2_3.java
@@ -7,11 +7,10 @@ public class Task13LoopIf1_2_3 {
 
     public static void lesson() {
         for (int i = 1; i <= 3; i++) {
+            System.out.println("Это " + i + "-й урок!");
             if (i <= 2) {
-                System.out.println("Это " + i + "-й урок!");
                 System.out.println("    Я выполнил все домашние задания " + i + "-го урока.");
             } else {
-                System.out.println("Это " + i + "-й урок!");
                 System.out.println("    Я не выполнил домашние задания " + i + "-го урока.");
             }
         }

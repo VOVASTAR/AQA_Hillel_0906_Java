87931e4fcda1838cb1bd07623d49d2041491f644
BOBA <v.starchevskyi@gmail.com>
2023-07-23 22:51:45 +0300

    recursion home + break/continue/return

--- src/ua/hillel/lessons/lesson12/Recursion.java
+++ src/ua/hillel/lessons/lesson12/Recursion.java
@@ -1,10 +1,19 @@
 package ua.hillel.lessons.lesson12;
 
 public class Recursion {
-static int factorialRecursion(int x){
-    int result;
-    result = factorialRecursion(x-1)*x;
-    return result;
-}
+    public static void main(String[] args) {
+        int a = factorialRecursion(4);
+        System.out.println(a);
+    }
+
+    static int factorialRecursion(int x) {
+        int result;
+        if (x < 1) {
+            return 1;
+        }
+        result = factorialRecursion(x - 2);
+        result *= x;
+        return result;
+    }
 
 }

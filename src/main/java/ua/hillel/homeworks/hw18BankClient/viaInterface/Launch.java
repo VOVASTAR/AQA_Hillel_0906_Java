a22d792f468724d2a8b4a2da41ddbd2e1b9e3a8b
vstarchevskyi <v.starchevskyi@zakaz.global>
2023-08-18 14:06:46 +0300

    hw18-add Launch Interface with methods

--- src/main/java/ua/hillel/homeworks/hw18BankClient/viaInterface/Launch.java
+++ src/main/java/ua/hillel/homeworks/hw18BankClient/viaInterface/Launch.java
@@ -4,10 +4,24 @@ public class Launch {
     public static void main(String[] args) {
 
         Client company = new LegalEntity(1200.5);
+        company.myBalance();
+        company.addMoney(2500);
+        company.takeMoney(1500);
+        company.myBalance();
+        company.aboutAccount();
 
-        Client fop = new IndividualBusinessman();
+        Client fop = new IndividualBusinessman(1000);
+        fop.myBalance();
+        fop.addMoney(200);
+        fop.takeMoney(300);
+        fop.myBalance();
+        fop.aboutAccount();
 
         Client person = new IndividualPerson(100);
-
+        person.myBalance();
+        person.addMoney(1600.5);
+        person.takeMoney(1300);
+        person.myBalance();
+        person.aboutAccount();
     }
 }

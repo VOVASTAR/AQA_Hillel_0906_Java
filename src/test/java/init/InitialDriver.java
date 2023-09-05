bf2d80e666c5e2b97b4c43c186ff9bd30541793d
vstarchevskyi <v.starchevskyi@zakaz.global>
2023-09-05 21:13:38 +0300

    lesson 24

---
+++ src/test/java/init/InitialDriver.java
@@ -0,0 +1,31 @@
+package init;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
+public class InitialDriver {
+
+    public WebDriver driver;
+    public WebDriverWait webDriverWait;
+
+    @BeforeTest
+    public void setupDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5000));
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+    }
+
+
+    @AfterTest
+    public void driverQuit() {
+        driver.quit();
+    }
+
+}

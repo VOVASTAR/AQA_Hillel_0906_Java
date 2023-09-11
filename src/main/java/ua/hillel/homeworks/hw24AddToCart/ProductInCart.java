91ca3aefae9b98bb69f3f30523b5dc992b6e3c0b
vstarchevskyi <v.starchevskyi@zakaz.global>
2023-09-11 13:36:43 +0300

    hw 24 add new line between steps

--- src/main/java/ua/hillel/homeworks/hw24AddToCart/ProductInCart.java
+++ src/main/java/ua/hillel/homeworks/hw24AddToCart/ProductInCart.java
@@ -32,29 +32,40 @@ public class ProductInCart {
         WebElement sectionLaptopAndComputers = driver
                 .findElement(By.xpath("//rz-main-page-sidebar//a[contains(@href,'computers-notebooks')]"));
         sectionLaptopAndComputers.click();
+
         WebElement sectionLaptops = driver
                 .findElement(By.xpath("//rz-list-tile//a[contains(@href,'notebooks')]"));
         sectionLaptops.click();
+
         WebElement firstLaptop = driver.findElement(By.xpath("//rz-catalog-tile"));
         firstLaptop.click();
+
         WebElement laptopTitle = driver.findElement(By.xpath("//rz-title-block//h1"));
         String titleText = laptopTitle.getAttribute("innerText");
+
         WebElement productInCart = driver.findElement(By.xpath("//rz-cart//rz-icon-badge"));
         String productInCartQuantity = productInCart.getAttribute("innerText");
-        Assert.assertEquals(productInCartQuantity,"","Cart doesn`t empty");
+        Assert.assertEquals(productInCartQuantity, "", "Cart doesn`t empty");
+
         WebElement buyBtn = driver.findElement(By.xpath("//rz-product-buy-btn//button"));
         buyBtn.click();
+
         WebElement closeCartPopup = driver.findElement(By.xpath("//button[@class='modal__close']"));
         closeCartPopup.click();
+
         WebElement stateInCart = driver.findElement(By.xpath("//button[contains(@class,'buy-button_state_in-cart')]"));
         Assert.assertTrue(stateInCart.isDisplayed(), "Item not in the cart");
         productInCartQuantity = productInCart.getText().trim();
+
         WebElement cartBtn = driver.findElement(By.xpath("//rz-cart/button"));
         cartBtn.click();
+
         WebElement laptopTitleInCart = driver.findElement(By.xpath("//a[@class='cart-product__title']"));
         String laptopTitleTextInCart = laptopTitleInCart.getAttribute("innerText");
+
         WebElement counterItemInCart = driver.findElement(By.xpath("//rz-cart-counter//input"));
         String quantityItemInCart = counterItemInCart.getAttribute("value");
+
         Assert.assertEquals(laptopTitleTextInCart, titleText, "Titles doesn't equals");
         Assert.assertEquals(quantityItemInCart, productInCartQuantity, "Quantity doesn`t equals");
     }
